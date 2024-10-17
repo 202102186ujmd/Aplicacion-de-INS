2248522e337eae763281a3127d434dd058c91a7f
Pedro Armando Navarrete Deodanes <202102186@ujmd.edu.sv>
2024-10-17 08:14:06 -0600

    Actualizacion

--- EventosINS/eventos/urls.py
+++ EventosINS/eventos/urls.py
@@ -1,11 +1,12 @@
 # eventos/urls.py
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import CustomLoginView, home, about_view    
+from .views import CustomLoginView, home, about_view, evento_detail    
 
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('', home, name='home'), #ruta para la pagina principal
     path('about/', about_view, name='about'),
+   path('evento/<int:id_evento>/', evento_detail, name='evento_detail'), 
 ]
--- EventosINS/eventos/views.py
+++ EventosINS/eventos/views.py
@@ -1,22 +1,38 @@
-# eventos/views.py
+# Importaciones necesarias
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import LoginForm
 from .models import Evento
+from datetime import datetime  # Para obtener la fecha actual
 
 # Vista para el login
 class CustomLoginView(LoginView):
-    template_name = 'eventos/login.html'  # Asegúrate de que este sea el archivo correcto
+    template_name = 'eventos/login.html'
     authentication_form = LoginForm
 
 # Vista para la página principal
 def home(request):
-    eventos = Evento.objects.filter(estado='activo')  # Obtener solo eventos activos
-    return render(request, 'eventos/home.html', {'eventos': eventos}) 
+    # Obtener la fecha actual
+    now = datetime.now()
 
-# Vista para la pagina de about
+    # Filtrar eventos que ocurren en el mes actual y con privacidad específica
+    eventos = Evento.objects.filter(
+        fecha__year=now.year,
+        fecha__month=now.month,
+        privacidad='publico',  # Cambia 'publico' por el valor de privacidad que desees
+        estado='activo'  # Asumiendo que solo mostramos eventos activos
+    )
+
+    return render(request, 'eventos/home.html', {'eventos': eventos})
+
+# Vista para la página de about
 def about_view(request):
-    return render(request, 'eventos/about.html') 
+    return render(request, 'eventos/about.html')
 
+# Vista para los detalles del evento
+def evento_detail(request, id_evento):
+    # Obtener el evento o mostrar un error 404 si no existe
+    evento = get_object_or_404(Evento, id_evento=id_evento)
 
-
+    # Renderizar la plantilla 'evento_detail.html' con los detalles del evento
+    return render(request, 'eventos/evento_detail.html', {'evento': evento})
